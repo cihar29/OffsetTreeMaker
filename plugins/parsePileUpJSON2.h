d1378dc679dc8453bd952cf3d96647938ecfc37c
Chad Harrington <cihar29@fnal.gov>
2016-06-16 14:56:15 -0500

    updated min-bias cross section and offers bril-calc or pileup JSON mu

--- plugins/parsePileUpJSON2.h
+++ plugins/parsePileUpJSON2.h
@@ -9,16 +9,63 @@
 
 using namespace std;
 
-map<int, map<int, double> > m_PU;
-const float MINBIAS_XS = 69000;
+map<int, map<int, float> > m_PU;
+const float MINBIAS_XS = 71300;
 
-double getAvgPU(int run, int ls) {
+float getAvgPU(int run, int ls) {
 
   return m_PU[run][ls];
 }
 
-int parsePileUpJSON2(string filename="pileup_JSON_1911.txt") {
+int parsePileUpJSON2(string filename="pileup_5_24_16.txt") {
+/*
+  //### Using Brilcalc ###//
+  cout << "Opening " << filename << "...";
 
+  string line;
+  ifstream file(filename);
+
+  if (file.is_open()){
+    cout << "ok" << endl;
+
+    //loop over lines in file
+    while ( getline(file,line) ){
+
+      string run_str, ls_str;
+      string str;
+      int delim_pos;
+      float PU = -1;
+
+      if ( line.at(0) != '#' ){
+
+        //loop over strings in line
+        for (int string_num=0; (delim_pos = line.find(",")) != -1; string_num++){
+
+          str = line.substr(0, delim_pos);
+          line.erase(0, delim_pos + 1);
+
+          if (string_num == 0)  //first string holds run number
+            run_str = str.substr(0, str.find(":"));
+
+          else if (string_num == 1) //second string has ls
+            ls_str = str.substr(0, str.find(":"));
+
+          else if (string_num == 7) //eighth string has pu
+            PU = stof( str );
+        }
+        int run = stoi( run_str );
+        int ls = stoi( ls_str );
+
+        m_PU[run][ls] = PU;
+      }
+    }
+    file.close();
+  }
+  else
+    cout << "Unable to open file" << endl;
+*/
+
+  //### Using Pileup JSON file ###//
   cout << "Minimum Bias Cross Section: " << MINBIAS_XS << endl;
   cout << "Opening " << filename << "...";
 
@@ -31,11 +78,9 @@ int parsePileUpJSON2(string filename="pileup_JSON_1911.txt") {
     //loop over lines in file
     while ( getline(file,line) ){
 
-      // brilcalc
-      // string run_str, ls_str;
       string str;
       int delim_pos, run, ls;
-      double PU = -1;
+      float PU = -1;
 
       if ( line.at(0) != '#' ){
 
@@ -45,16 +90,6 @@ int parsePileUpJSON2(string filename="pileup_JSON_1911.txt") {
           str = line.substr(0, delim_pos);
           line.erase(0, delim_pos + 1);
 
-          // brilcalc USE COMMA
-          //if (string_num == 0)  //first string holds run number
-          //  run_str = str.substr(0, str.find(":"));
-
-          //else if (string_num == 1) //second string has ls
-          //  ls_str = str.substr(0, str.find(":"));
-
-          //else if (string_num == 7) //eighth string has pu
-          //  PU = stod( str );
-
           if (string_num == 0)  //first string holds run number
             run = stoi( str );
 
@@ -62,11 +97,6 @@ int parsePileUpJSON2(string filename="pileup_JSON_1911.txt") {
             ls = stoi( str );
         }
         PU = stod( line ) * MINBIAS_XS;
-
-        // brilcalc
-        //int run = stoi( run_str );
-        //int ls = stoi( ls_str );
-
         m_PU[run][ls] = PU;
       }
     }
